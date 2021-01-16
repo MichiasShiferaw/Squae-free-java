c7fe4a55dfb2019ccdd38386ee2bb0e39601e88b
Michias Shiferaw <68077955+MichiasShiferaw@users.noreply.github.com>
2021-01-16 16:24:50 -0500

    Intro

---
+++ squarefree.py
@@ -0,0 +1,6 @@
+def squarefree(s):
+    """This is a function that has one parameter, s, where s is a string. The function returns
+    True if s is squarefree and False otherwise. 
+    Precondition:
+    s: is a string
+    Return boolean"""
